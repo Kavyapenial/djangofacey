9fbda09a6ba4bf70b890667081617a00c5a86968
kavya jacob <kavyanoel@gmail.com>
2020-01-18 11:30:33 +0530

    table teacher in user_auth

--- user_auth/models.py
+++ user_auth/models.py
@@ -2,8 +2,12 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+
 class Teacher(models.Model):
-    hotel = models.ForeignKey(HotelDetail, on_delete=models.CASCADE)
+    t_id = models.IntegerField(default=0)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    indate = models.DateField(default=0)
-    outdate = models.DateField(default=0)
+    name = models.CharField(max_length=50, default=None)
+    pic = models.ImageField(default=None)
+    designation = models.CharField(max_length=50, default=None)
+    email = models.ForeignKey(max_length=50, default=None)
